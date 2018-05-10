0c5de5e3e368158dd4368599484d0fb644884a62
rootkc <info@kennethchristensen.dk>
2018-05-10 18:22:07 +0200

    go to paper trading

--- src/config/index.js
+++ src/config/index.js
@@ -4,13 +4,14 @@ import { sessionQuery } from '../db'
 const { BITTREX_API_KEY, BITTREX_API_SECRET } = process.env
 
 bittrex.options({
+  verbose: true,
   apikey: BITTREX_API_KEY,
   apisecret: BITTREX_API_SECRET,
 })
 
 export const TIME_FRAME = 14400
 export const BACKTEST_DAYS = 200
-export const BACKTEST = true
+export const BACKTEST = false
 export const PAPER_TRADE = true
 export const PAIR = 'BTC-OMG'
 export let SESSION_ID = null // eslint-disable-line import/no-mutable-exports
