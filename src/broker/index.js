cd9039f1d81c82ea3524ba02337011465773659b
rootkc <info@kennethchristensen.dk>
2018-08-29 20:29:24 +0200

    only call executioner if PAPER_TRADE is false

--- src/broker/index.js
+++ src/broker/index.js
@@ -1,12 +1,12 @@
 import moment from 'moment'
 import { signalQuery } from '../db'
-import { SESSION_ID } from '../config'
+import { SESSION_ID, PAPER_TRADE } from '../config'
 import execute from './executor/'
 
 // TODO takes data from market and returns an order event
 const executeOrder = (marketData) => {
   const candleDate = moment(marketData.date * 1000).toDate()
-  execute(marketData)
+  if (!PAPER_TRADE) execute(marketData).subscribe(data => console.log(data))
   return signalQuery([marketData.type, candleDate, SESSION_ID])
 }
 
