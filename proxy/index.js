441dc00cdda0b0339fc524c61a9cda59520ef0e6
rootkc <info@kennethchristensen.dk>
2019-05-05 14:50:15 +0200

    add mock server and http support and try to get sockets to proxy

--- proxy/index.js
+++ proxy/index.js
@@ -1,27 +1,40 @@
+const http = require('http')
 const https = require('https')
 const httpProxy = require('http-proxy')
 const pem = require('pem')
 
-pem.createCertificate({ days: 1, selfSigned: true }, (err, keys) => {
-  if (err) {
-    throw err
+pem.createCertificate({ days: 1, selfSigned: true }, (certErr, keys) => {
+  if (certErr) {
+    throw certErr
   }
   const httpsOptions = {
     key: keys.serviceKey,
     cert: keys.certificate,
   }
 
-  httpProxy.createServer({
-    target: 'https://localhost:8001',
+  const proxy = httpProxy.createServer({
     ssl: httpsOptions,
-    changeOrigin: true,
-    secure: true,
-  }).listen(8010)
+    ws: true,
+    secure: false,
+  })
 
-  https.createServer(httpsOptions, (req, res) => {
-    console.log('here here')
-    res.writeHead(200, { 'Content-Type': 'text/plain' })
-    res.write('hello https\n')
-    res.end()
-  }).listen(8001)
+  const secureServer = https.createServer(httpsOptions, (req, res) => {
+    console.log('here here http secure', req.url)
+    proxy.web(req, res, { target: 'http://mock:1880' })
+  })
+
+  const server = http.createServer((req, res) => {
+    console.log('here here http non secure', req.url)
+
+    proxy.web(req, res, { target: 'http://mock:1880' })
+  })
+
+  server.on('upgrade', (req, socket, head) => {
+    console.log('here here socket', req, socket, head)
+    proxy.ws(req, socket, { target: 'ws://mock:1880/signalr' })
+  })
+
+  proxy.listen(8010)
+  secureServer.listen(8001)
+  server.listen(8000)
 })
--- src/index.js
+++ src/index.js
@@ -4,8 +4,34 @@ import getMarketData from 'Core/market'
 import runStrategy from 'Core/strategy'
 import portfolioManager from 'Core/portfolio'
 import executeOrder from 'Core/broker'
+import axios from 'axios'
+
+const WebSocket = require('ws');
+ 
+const ws = new WebSocket('ws://echo.websocket.org/', {
+  origin: 'http://websocket.org'
+});
+ 
+ws.on('open', function open() {
+  console.log('connected');
+  ws.send(Date.now());
+});
+ 
+ws.on('close', function close() {
+  console.log('disconnected');
+});
+ 
+ws.on('message', function incoming(data) {
+  console.log(data)
+  console.log(`Roundtrip time: ${Date.now() - data} ms`);
+ 
+  setTimeout(function timeout() {
+    ws.send(Date.now());
+  }, 500);
+});
 
 const runBot = async (name) => {
+  await axios.get('http://example.org')
   // we start a new session and await a sessionId
   await newSession(name)
 
